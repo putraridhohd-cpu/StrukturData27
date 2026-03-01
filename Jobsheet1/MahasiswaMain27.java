183c2f502f0693478252ddf78f17254f645c405e
valentridho <putraridhohd@gmail.com>
2026-03-01 23:11:11 +0700

    Modifkasi percobaan 2

--- Jobsheet1/MahasiswaMain27.java
+++ Jobsheet1/MahasiswaMain27.java
@@ -4,7 +4,7 @@ public class MahasiswaMain27 {
     public static void main(String[] args) {
     Mahasiswa27 mhs1 = new Mahasiswa27();
     mhs1.nama = "Valent ridho putra santoso";
-    mhs1.NIM = "123456789";
+    mhs1.NIM = "254107020050";
     mhs1.kelas = "TI 1-H";
     mhs1.ipk = 3.5;
 
